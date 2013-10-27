da387d98ac4c4a1632518e76491bcecba91f71c4
Menno de Brie <menno@paperchip.nl>
2013-10-27 16:47:25 +0100

    fixed typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,resp) {
         resp.writeHead(200, {"Content-Type": "text/plain"});
-        resp.write("Hello World, edited from my machine and pushed throug Github");
+        resp.write("Hello World, edited from my machine and pushed through Github");
         resp.end();
 
         console.log("sample output to console");
